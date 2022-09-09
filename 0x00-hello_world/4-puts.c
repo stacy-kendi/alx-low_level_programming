01aafb3ef38024d3051e33e9ea22c58666e6b626
root <stacykendi57@gmail.comgit config --global user.name stacy-kendi>
2022-09-08 19:37:28 -0700

    C program that prints exactly a text followed by new line using the function puts and program ends with value 0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
-/*print a text and the program to return a value of 0
+/*
+ * print a text and the program to return a value of 0
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
