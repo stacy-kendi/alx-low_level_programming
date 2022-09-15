8cbcad006d1f839a796d5e0928d57c9ebc848f19
root <stacykendi57@gmail.comgit config --global user.name stacy-kendi>
2022-09-15 12:49:48 -0700

    test if integer is positive or negative number

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - tests if an integer is negative or positive
+ * Return: 0 as success
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
