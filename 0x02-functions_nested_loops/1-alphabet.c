329f362367c366b34374777121e117eb72decba9
root <stacykendi57@gmail.comgit config --global user.name stacy-kendi>
2022-09-15 08:23:38 -0700

    print letters in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabets in lowercase
+ * Return: 0
+ */
+
+int print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	return (0);
+}
