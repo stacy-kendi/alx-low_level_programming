4901579fe749a2d09801f25062a291ac0bb721db
root <stacykendi57@gmail.comgit config --global user.name stacy-kendi>
2022-09-15 11:00:59 -0700

    adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @a: first number input
+ * @b: second number input
+ * Return: the result of a + b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
