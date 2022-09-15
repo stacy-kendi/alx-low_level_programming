8cdc3e92b5220ab4ed4734844e85c57c7f452484
root <stacykendi57@gmail.comgit config --global user.name stacy-kendi>
2022-09-15 11:41:04 -0700

    computes and prints the sum of all the multiples of 3 or 5 below 1024 (excluded), followed by a new line

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - computes and prints the sum of all
+ * multiples of 3 or 5 below 1024 (excluded)
+ * Return: 0
+ */
+
+int main(void)
+{
+	int sum = 0;
+	int i  = 0;
+
+	while (i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum += i;
+			i++;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
