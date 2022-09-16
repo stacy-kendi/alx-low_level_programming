39e6c97ffde3288be78989e62ace1cdb79f8daf8
root <stacykendi57@gmail.comgit config --global user.name stacy-kendi>
2022-09-15 18:32:26 -0700

    multiple of two integers a*b

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  mul - multiplies two integers
+ *  @a: first input
+ *  @b: second input
+ *  Return: result of multiplying two inputs
+ */
+
+int mul(int a, int b)
+{
+	int answer;
+
+	answer = a * b;
+
+	return (answer);
+}
