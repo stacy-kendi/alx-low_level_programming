919bfe6863e6c665d132e50e0e190048564ebefc
root <stacykendi57@gmail.comgit config --global user.name stacy-kendi>
2022-09-15 18:37:35 -0700

    prints the numbers, from 0 to 9, followed by a new line

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0-9
+ * Return: 0-9
+ */
+
+void print_numbers(void)
+{
+	int j;
+
+	for (j = 0; j <= 9; j++)
+	{
+		_putchar(j);
+	}
+
+	_putchar('\n');
+}
