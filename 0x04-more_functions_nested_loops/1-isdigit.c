1ed554117efbc1cb9d12915e8e0d5dc12aa2152e
root <stacykendi57@gmail.comgit config --global user.name stacy-kendi>
2022-09-15 18:27:42 -0700

    checks for a digit (0 through 9) if c is a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit
+ * @c: input
+ * Return: 1 if is a digit else 0
+ */
+
+int _isdigit(int c)
+{
+	if ((c >= 48) && (c <= 57))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
