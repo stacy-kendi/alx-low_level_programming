68eb65f1d4be9abd340d8d20e8f827cc665d21a6
root <stacykendi57@gmail.comgit config --global user.name stacy-kendi>
2022-09-15 18:20:21 -0700

    Check if c is uppercase using isupper function

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *  _isupper - Check for uppercase character
+ *  @c: input
+ *  Return: 1 if c is uppercase else 0
+ */
+
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
