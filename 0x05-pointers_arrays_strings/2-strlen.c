eb8d794cecc3147c1a96323c2d43677c594ab3f9
root <stacykendi57@gmail.comgit config --global user.name stacy-kendi>
2022-09-19 13:13:59 -0700

    Write a function that returns the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen -  function that returns the length of a string
+ * @s: char input
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	int i = 1;
+	sum = 0;
+	char point = s[0];
+
+	while (point != '\0')
+	{
+		sum++;
+		point = s[i++];
+	}
+	return (sum);
+}
