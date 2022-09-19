784787a7c0eb33dba0c3f6c22c53190527afa0e7
root <stacykendi57@gmail.comgit config --global user.name stacy-kendi>
2022-09-19 12:57:51 -0700

    a function that takes a pointer to an int as parameter and updates the value it points to to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 /**
- * reset_to_98 -  function that takes a pointer to an int as parameter & updates
- * the value it points to to 98
+ * reset_to_98 -  function that takes a pointer to an int as parameter
+ * & updates the value it points to to 98
  * @n: input
  * Return: n
  */
