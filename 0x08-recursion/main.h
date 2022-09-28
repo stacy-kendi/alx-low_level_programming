269b9386f8aedc30e84206ccc8ace7e289e5f379
root <stacykendi57@gmail.comgit config --global user.name stacy-kendi>
2022-09-28 15:41:47 -0700

    editing the functions to squared and recursivefn

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -15,11 +15,11 @@ int _pow_recursion(int x, int y);
 
 int _sqrt_recursion(int n);
 
-int halp(int c, int i);
+int squared(int c, int i);
 
 int is_prime_number(int n);
 
-int hai(int n, int d);
+int recursivefn(int n, int d);
 
 int _sqrt(int x, int i);
 
