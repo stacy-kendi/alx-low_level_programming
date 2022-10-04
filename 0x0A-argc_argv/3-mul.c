586ade702fdbe870b6a0b0226cc0fc30eb2a15ad
root <stacykendi57@gmail.comgit config --global user.name stacy-kendi>
2022-10-04 13:02:37 -0700

    program that multiplies two numbers If the program does not receive two arguments, your program should print Error

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiply 2 numbers passed to main
+ * print the result of the multiplication, followed by a new line
+ * assume that the two numbers and result can be stored as integer
+ * program does not receive two arguments print error
+ * Return: 1 as error
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	if (argv[1] && argv[2])
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
+	}
+	else
+		printf("Error\n");
+
+	return (1);
+}
