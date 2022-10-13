f5d2681c769aa6f36ab6430f662dbc89c9b88377
root <stacykendi57@gmail.comgit config --global user.name stacy-kendi>
2022-10-13 04:58:31 -0700

    function that prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - a function to print a name from function pointer 
+ * @name: char string
+ * @f: function pointer that takes a string argument
+ * Return: void with a name
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
