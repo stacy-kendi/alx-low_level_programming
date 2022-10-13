b1f7a39bde15a6bf4123a7ada2b0634afa6b93f5
root <stacykendi57@gmail.comgit config --global user.name stacy-kendi>
2022-10-13 05:47:02 -0700

    program that performs simple operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add: sum of a and b
+ * @a: input one
+ * @b: input two
+ * Return: sum of a and b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub: difference or subtract of a and b
+ * @a: input one
+ * @b: input two
+ * Return: difference of a and b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul: multiplication of a and b
+ * @a: input one
+ * @b: input two
+ * Return: multiplication of a and b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div: result of dividing a and b
+ * @a: input one
+ * @b: input two
+ * Return: result of dividing a and b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod: remainder after dividing a and b
+ * @a: input one
+ * @b: input two
+ * Return: resulting remainder modulus
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
