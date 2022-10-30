ac7b75e0b28bb8933f88f66c799ac768d07f68b1
root <stacykendi57@gmail.comgit config --global user.name stacy-kendi>
2022-10-30 09:49:55 -0700

    program that prints the name of the file it was compiled from, followed by a new line

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry Print the name of the file it was compiled from
+ * Return: 0 as success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
