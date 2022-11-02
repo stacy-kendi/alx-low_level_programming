6be6dbc156cc4c3bd479e36b68be3f0da5bf6b8d
root <stacykendi57@gmail.comgit config --global user.name stacy-kendi>
2022-11-02 11:48:46 -0700

    function that creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * create_file - create a file with read/write access for user
+ * @filename: name of file to create
+ * @text_content: null terminated string to write to file
+ * The created file must have those permissions: rw-------
+ * and if file exists maintain permissions, if file exists truncate it
+ * if filename is NULL return -1 if text_content is NULL create an empty file
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, rstatus, i;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (i = 0; text_content[i] != '\0'; i++)
+			;
+		rstatus = write(fd, text_content, i);
+		if (rstatus == -1)
+			return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
